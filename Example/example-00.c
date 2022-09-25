c62dd73c911adc68325446f1fa5409c3f28a655f
Öner Morkoç <79011662+onermorkoc@users.noreply.github.com>
2022-09-25 04:06:31 +0300

    Update example-00.c

--- Example/example-00.c
+++ Example/example-00.c
@@ -14,7 +14,7 @@ int main(void)
 {
 	/* ==> mlx_init() Fonksiyonu <==
 
-	Minilibx kütüphanesini başlatıyor ve void bir pointer döndürüyor. Dönen void pointerı
+	Minilibx kütüphanesini başlatır ve void bir pointer döndürür. Dönen void pointerı
 	neredeyse kütüphanenin tüm fonksıyonlara parametre olarak yolluyoruz.
 
 	*/
@@ -25,9 +25,9 @@ int main(void)
 
 	/* ==> mlx_new_window() Fonsiyonu <==
 
-	Siyah içi boş bir pencere oluşturuyor. Parametre olarak mlx_ptr, pencerenin genişliği için ise int width,
-	yüksekliği için int height alıyor. Son olarak da pencere adı alıyor(const char bir dizi olarak) ve 
-	bize void bir win_ptr pointer döndürüyor. mlx_ptr gibi hemen hemen kütüphanenin tüm fonksiyonlara
+	Siyah içi boş bir pencere oluşturur. Parametre olarak mlx_ptr, pencerenin genişliği için int width,
+	yüksekliği için int height ve son olarak pencere adı alır(const char bir dizi olarak)
+	Daha sonra fonksiyon bize void bir win_ptr pointer döndürür. mlx_ptr gibi hemen hemen kütüphanenin tüm fonksiyonlara
 	parametre olarak yolluyoruz.
 
 	*/
@@ -38,8 +38,8 @@ int main(void)
 
 	/* ==> mlx_xpm_file_to_image() Fonksiyonu <==
 
-	xmp uzantılı resimleri okuyor ve bir pointer döndürüyor (dönen pointeri ekrana basması için mlx_put_image_to_window() fonksiyona parametre olarak yollucağız)
-	Parametre olarak mlx_ptr, xpm dosyasının konumu ve iki tane değeri verilmemiş int adresi istiyor. Eğer okuyamazsa null döndürüyor. Resimler genellikle png veya jpg
+	xmp uzantılı resimleri okur ve bir pointer döndürür (dönen pointeri ekrana basması için mlx_put_image_to_window() fonksiyona parametre olarak yollucağız)
+	Parametre olarak mlx_ptr, xpm dosyasının konumu ve iki tane değeri verilmemiş int adresi veriyoruz. Eğer okuyamazsa null döndürür. Resimler genellikle png veya jpg
 	olur xpm çevirmek için bu siteyi kullanabilirsiniz: https://convertio.co/tr/png-xpm/
 
 	*/
@@ -59,8 +59,8 @@ int main(void)
 
 	/* ==> mlx_put_image_to_window() Fonsiyonu <==
 
-	Resimlerı ekrana basmamıza yarıyor. Parametre olarak mlx_ptr, win_ptr, img_ptr ve resmin pencerenin hangi konuma basılacagını belirtmek için
-	bir x y kordınatı alıyor örnek olarak : (0, 0) noktası verelim
+	Resimlerı ekrana basmamıza yarar. Parametre olarak mlx_ptr, win_ptr, img_ptr ve resmin pencerenin hangi konuma basılacagını belirtmek için
+	bir x y kordınatı alır örnek olarak : (0, 0) noktası verelim
 
 	*/
 	mlx_put_image_to_window(mlx_ptr, win_ptr, img_ptr, 0, 0);
@@ -70,7 +70,7 @@ int main(void)
 
 	/* ==> mlx_clear_window() Fonksiyonu <==
 
-	Pencereye basılmıs tüm resimleri, pixelleri temizliyor parametre olarak mlx_ptr ve win_ptr alıyor.
+	Pencereye basılmıs tüm resimleri, pixelleri temizler parametre olarak mlx_ptr ve win_ptr alır.
 
 	*/
 	write(1, "2 saniye içinde pencere temizlenecek...\n", 41);
@@ -82,7 +82,7 @@ int main(void)
 
 	/* ==> mlx_destroy_window() Fonksiyonu
 
-	Pencereyi yok ediyor parametre olarak mlx_ptr ve win_ptr alıyor.
+	Pencereyi yok eder parametre olarak mlx_ptr ve win_ptr alır.
 
 	*/
 	write(1, "2 saniye içinde pencere yok edilecek...\n", 41);
@@ -106,7 +106,7 @@ int main(void)
 
 	/* ==> mlx_destroy_display() Fonksiyonu <==
 
-	Kütüphaneyi freelemek için kullanılır. Parametre olarak mlx_ptr alıyor.
+	Kütüphaneyi freelemek için kullanılır. Parametre olarak mlx_ptr alır.
 
 	*/
 	write(1, "2 saniye içinde kütüphane freelenecek...\n", 44);
@@ -118,7 +118,7 @@ int main(void)
 
 	/* ==> mlx_loop Fonksiyonu <==
 
-	Pencerenin açık kalmasını sağlıyor. Parametre olarak mlx_ptr alıyor.
+	Pencerenin açık kalmasını sağlar. Parametre olarak mlx_ptr alır.
 
 	*/
 	mlx_loop(mlx_ptr);
